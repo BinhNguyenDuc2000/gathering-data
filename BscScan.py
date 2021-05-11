ef5b0f82a5516e1bd5d43f037625b62173d3f6b9
bcoldx <76272856+bcoldx@users.noreply.github.com>
2021-05-11 09:05:05 +0700

    updating fico

--- BscScan.py
+++ BscScan.py
@@ -6,6 +6,7 @@ from datetime import date
 import math
 
 account_list = []
+er = 0
 
 def get_average_transaction(address="0x0000000000000000000000000000000000001004"):
     a = "https://api.bscscan.com/api?module=account&action=txlist&address="
@@ -14,7 +15,15 @@ def get_average_transaction(address="0x0000000000000000000000000000000000001004"
     record_number = "30"
     api_token = "EHK917H6T8GF36B3H62T5MSWDMS4YT6HUA"
     rq = a + address + b + record_number + c + api_token
-    response = requests.get(rq)
+    global er
+    try:
+        response = requests.get(rq)
+    except requests.ConnectionError: 
+        er = 1
+        return 0
+    if response.status_code != 200:
+        er = 1
+        return 0
     raw = response.json()
     i = 0
     total = 0
@@ -35,11 +44,21 @@ def get_time(address="0x0000000000000000000000000000000000001004"):
     record_number = "1"
     api_token = "EHK917H6T8GF36B3H62T5MSWDMS4YT6HUA"
     rq = a + address + b + record_number + c + api_token
-    response = requests.get(rq)
+    global er
+    try:
+        response = requests.get(rq)
+    except requests.ConnectionError:
+        er = 1
+        return 0
+    if response.status_code != 200:
+        er = 1
+        return 0
     raw = response.json()
     if len(raw['result'])==0:
+        er = 1
         return 0
     if not (str(raw['result'][0]['timeStamp']).isnumeric):
+        er = 1
         return 0
     time_stamp = int(raw['result'][0]['timeStamp'])
     old_date = datetime.fromtimestamp(time_stamp)
@@ -52,9 +71,18 @@ def get_balance(address="0x0000000000000000000000000000000000001004"):
     b = "&tag=latest&sort=asc&apikey"
     api_token = "EHK917H6T8GF36B3H62T5MSWDMS4YT6HUA"
     rq = a + address + b + api_token
-    response = requests.get(rq)
+    global er
+    try:
+        response = requests.get(rq)
+    except requests.ConnectionError:
+        er = 1
+        return 0
+    if response.status_code != 200:
+        er = 1
+        return 0
     raw = response.json()
     if not (str(raw['result']).isnumeric()):
+        er = 1
         return 0
     result = int(raw['result'])
     if result == 0:
@@ -72,20 +100,23 @@ def get_fico(address="0x0000000000000000000000000000000000001004"):
     time.sleep(1)
     print (history)
     fico =average_transaction*0.35 + balance*0.3 + 0.35*history
+    global er
     info = {
         "address" : address,
         "average_transaction" : average_transaction,
         "balance" : balance,
         "history" : history,
-        "fico" : int(fico)
+        "fico" : int(fico),
+        "error" : er
     }
+    er = 0
     account_list.append(info)
     with open('fico.json', 'w') as outfile:
-    json.dump(account_list, outfile,indent=4)
+        json.dump(account_list, outfile,indent=4)
     return int(fico)
 
 fo = open('acc.json','r')
 acc = json.load(fo)
 fo.close()
-for i in range(10):
+for i in acc:
     get_fico(acc[str(i)])
--- ReadUrl.py
+++
@@ -1,3 +0,0 @@
-import urllib.request
-page = urllib.request.urlopen('https://bscscan.com/accounts')
-print(page.read())
